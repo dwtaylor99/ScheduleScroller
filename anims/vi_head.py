d2adfae51d0ee6cc390278d24e13467e5c766e3f
David Taylor <dwtaylor99@gmail.com>
2025-01-22 14:39:36 -0500

    Vi's head floats a little longer

--- anims/vi_head.py
+++ anims/vi_head.py
@@ -13,6 +13,8 @@ class ViHead(FunBase):
         self.y = random.randrange(100, self.screen.get_height() // 2 - 200)
         self.vel_x = (2 - (random.randrange(0, 4))) * 0.2
         self.vel_y = (2 - (random.randrange(0, 4))) * 0.2
+        self.save_x = 0
+        self.save_y = 0
         self.alpha = 0
 
     def animate(self):
@@ -21,9 +23,17 @@ class ViHead(FunBase):
             super().animate()
             self.alpha += 1
             if self.alpha >= 220:
+                self.save_x = self.x
+                self.save_y = self.y
                 self.anim_step += 1
 
         elif self.anim_step == 2:
+            super().animate()
+
+            if abs(self.save_x - self.x) >= 50 or abs(self.save_y - self.y) >= 50:
+                self.anim_step += 1
+
+        elif self.anim_step == 3:
             self.img.set_alpha(self.alpha)
             super().animate()
             self.alpha -= 1
