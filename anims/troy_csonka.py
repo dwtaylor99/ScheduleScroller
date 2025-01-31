f89909b040e91978c93f6f958a93f6fc101e0041
David Taylor <dwtaylor99@gmail.com>
2025-01-31 15:08:43 -0500

    added delay to troy and zap anims

--- anims/troy_csonka.py
+++ anims/troy_csonka.py
@@ -2,7 +2,12 @@ import random
 
 import pygame
 
+import triviavox
 from funbase import FunBase
+
+
+local_clock = pygame.time.Clock()
+delta = 0
 
 
 class TroyCsonka(FunBase):
@@ -17,6 +22,8 @@ class TroyCsonka(FunBase):
         self.alpha = 0
 
     def animate(self):
+        global delta
+
         if self.anim_step == 1:
             super().animate()
             if self.y <= (self.screen.get_height() // 2) - self.img.get_height():
@@ -30,9 +37,18 @@ class TroyCsonka(FunBase):
             self.screen.blit(self.img2, (self.x + 200, self.y - 180))
             self.alpha += 1
             if self.alpha >= 255:
+                self.alpha = 255
                 self.anim_step += 1
 
         elif self.anim_step == 3:
+            super().animate()
+            self.screen.blit(self.img2, (self.x + 200, self.y - 180))
+
+            delta += local_clock.tick(triviavox.FPS) / 1000
+            if delta >= 15:
+                self.anim_step += 1
+
+        elif self.anim_step == 4:
             super().animate()
             self.img2.set_alpha(self.alpha)
             self.screen.blit(self.img2, (self.x + 200, self.y - 180))
@@ -41,7 +57,7 @@ class TroyCsonka(FunBase):
                 self.vel_y = 1
                 self.anim_step += 1
 
-        elif self.anim_step == 4:
+        elif self.anim_step == 5:
             super().animate()
             if self.y > self.screen.get_height() / 2:
                 self.anim_step = 0
