3a0932040ae777873044b6602e462b6c9de990d6
David Taylor <dwtaylor99@gmail.com>
2025-02-02 19:17:31 -0500

    added alpha to Gamera trails

--- anims/gamera.py
+++ anims/gamera.py
@@ -8,7 +8,7 @@ from funbase import FunBase
 class Gamera(FunBase):
     def __init__(self, screen):
         super().__init__(screen)
-        self.img = pygame.image.load('images/fun/gamera.png').convert_alpha()
+        self.img = pygame.image.load('images/fun/gamera2.png').convert_alpha()
         self.x = -(self.img.get_width()) - 10
         self.y = random.randrange(30, 450)
         self.vel_x = 4
